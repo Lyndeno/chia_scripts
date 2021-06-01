d181d64e21797fd847d3c3796fc41cf848932cb5
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-01 16:16:43 -0600

    Changed some paths

--- plot.sh
+++ plot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 start=$(date +%s)
-notify-send "Hello $1"
-# chia plots create -k 32 -b 3390 -r 2 -t /chia/temp -2 /chia/temp2 -d /chia/final -u 128
+#notify-send "Hello $1"
+chia plots create -k 32 -b 3390 -r 2 -t /chia/temp -2 /chia/temp2 -d /chia/matt1 -u 128
 end=$(date +%s)
 elapsed=$(($end-$start))
 notify-send "Plot $1 Finished" "Took $elapsed seconds."
